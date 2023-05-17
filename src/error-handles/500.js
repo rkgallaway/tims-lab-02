2eaae89f28cb8100ce7202487055060e322e06f4
Tim Maupin <timmaupin@live.com>
2023-05-16 22:52:58 -0500

    working through the code

---
+++ src/error-handles/500.js
@@ -0,0 +1,15 @@
+'use stirct';
+
+
+module.exports = (error, request, response, next) => {
+  const errorMessage = typeof(error) === 'string' ? error : error.message;
+  response.status(500).send ({
+    error: 500,
+    route: request.path,
+    query: request.query,
+    path: request.params,
+    body: request.body,
+    message: `Server Error: ${errorMessage}`,
+  });
+};
+
---
+++ src/middleware/validator.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+const { describe } = require('node:test');
+const validator = require('./index');
+
+
+describe
+
+
